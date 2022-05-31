148333032c573b642389b413ccd6a52a5f2c6f87
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2022-05-31 17:22:14 +0300

    Commit

--- js/2-module.js/02-for-loop.js
+++ js/2-module.js/02-for-loop.js
@@ -10,3 +10,36 @@
 // }
 
 
+// let counter = 0;
+
+// while (counter < 10) {
+//   console.log("counter: ", counter);
+//   counter += 1;
+// }
+
+// let clientCounter = 18;
+// const maxClients = 25;
+
+// while (clientCounter < maxClients) {
+//   console.log(clientCounter);
+//   clientCounter += 1;
+// }
+
+// let password = "";
+
+// do {
+//   password = prompt("Введіть пароль довший 4-х символів", "");
+// } while (password.length < 5);
+
+// console.log("Ввели пароль: ", password);
+
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+    let message;
+    const totalPrice = pricePerDroid * orderedQuantity;
+  if(totalPrice <= customerCredits){
+    message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left.`;
+  } else {
+    message = "Insufficient funds!";
+  }
+    return message;
+  }
